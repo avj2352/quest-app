ce82dbf64c025641b229ad11951fe335f4377d33
Jingade <pramod.jingade@philips.com>
2019-07-15 23:06:42 +0530

    Reset all frontend framework. start working with booststrap

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './views/dashboard/Dashboard.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/views/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Dashboard = (props) => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+};
+
+export default Dashboard;
